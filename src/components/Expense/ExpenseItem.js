502fb45d56e3fec393dc4240e5eff4d30bf0b1f9
Pratik Hati <pratikhati.work@gmail.com>
2022-05-08 16:51:12 +0530

    Concept of State

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-import React from "react";
+import React,{ useState} from "react";    //{ useState} is used for state concept if.e to reload a web page after being rendered
 
 function ExpenseItem(props) {
   
+  const [title, setTitle] = useState(props.title);    //important as this array should have exactly 2 elements
+                                                      //one before update, another after update
+
   function myclick(){
-    console.log('Clicked');
+    setTitle('Updated!');       //when clicked, value will be changes as ExpenseItem() will reexecute 
+    console.log(title);
   }
 
   return (
@@ -14,7 +18,7 @@ function ExpenseItem(props) {
     <Card className='expense-item'>              {/* Card is a custom component as a result 'Card's css will take precidence than 'expense-item' css*/}
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={myclick}>Click here</button>
