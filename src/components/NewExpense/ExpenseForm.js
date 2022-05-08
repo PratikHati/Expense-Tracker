0fdd4541c953f71cc350f29034cd5c2848ed41ca
Pratik Hati <pratikhati.work@gmail.com>
2022-05-08 20:48:32 +0530

    Used one state instead of 3 diff states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,57 @@
 import './ExpenseForm.css'
-import React from 'react'
+import React, {useState} from 'react'
 
 function ExpenseForm() {
 
+    // const [enteredTitle, changedEnteredTitle] = useState('');
+    // const [enteredAmount,EnteredAmount] = useState('')
+    // const [enteredDate,EnteredDate] = useState('')
+
+    const [enteredValue, changedValue] = useState({
+        enteredTitle:'',
+        enteredAmount:'',
+        enteredDate:''
+    });
+
     const titleChangeHandler = (event)=>{
-        console.log(event.target.value);
+        //changedEnteredTitle(event.target.value);
+        changedValue({
+            ...enteredValue,                    //keep rest 2 as it is
+            enteredTitle: event.target.value    //update with new value
+        });
     };
+
+    const amountChangeHandler = (event)=>{
+        //EnteredAmount(event.target.value);
+        changedValue({
+            ...enteredValue,                    //keep rest 2 as it is
+            enteredAmount: event.target.value    //update with new value
+        });
+    };
+
+    const dateChangeHandler = (event)=>{
+        //EnteredDate(event.target.value);
+        changedValue({
+            ...enteredValue,                    //keep rest 2 as it is
+            enteredDate: event.target.value    //update with new value
+        });
+    };
+
 
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__controls'>
-                    <label>Title</label>
+                    <label>enteredTitle</label>
                     <input type='text' id='ip' onChange={titleChangeHandler}></input>
                 </div>
                 <div className='new-expense__controls'>
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01" id='ip'></input>
+                    <input type='number' min="0.01" step="0.01" id='ip' onChange={amountChangeHandler}></input>
                 </div>
                 <div className='new-expense__controls'>
                     <label>Date</label>
-                    <input type='date' min="2022-01-01" max="2025-01-01" id='ip'></input>
+                    <input type='date' min="2022-01-01" max="2025-01-01" id='ip' onChange={dateChangeHandler}></input>
                 </div>
             </div>
             <div className='new-expense__actions'>
