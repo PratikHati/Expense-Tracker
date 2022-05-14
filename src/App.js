ee23e36f2c46c67a92f63957df586d8b7d9918a6
Pratik Hati <pratikhati.work@gmail.com>
2022-05-14 21:34:41 +0530

    StatefulList

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import Expenses from "./components/Expense/Expense";
-import React from "react";
+import React,{useState} from "react";
 import NewExpense from './components/NewExpense/NewExpense'
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
 
-  const myfunc = (fromNewExpense) =>{
-      console.log('data received in App.js');   //now it will print here instead of NewExpress.js
-      console.log(fromNewExpense);
-  }
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const myfunc = (expenses) =>{   //"expenses" is a new object came from child NewExpense.js to add in current list
+                                  // console.log('data received in App.js');   //now it will print here instead of NewExpress.js
+                                  // console.log(fromNewExpense);
+    
+
+    //---------------important -------------------"setExpenses" function can be used when list must have new snapshots along with older snapshots
+
+    setExpenses((prevState) =>{             //"prevState" is already existing list objects
+      return [expenses,...prevState];       //[newonj,oldobjlist]
+    });
+  };
+
   return (
 
     // JSX SYNTAX
